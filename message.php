ebbc79213d027ad353e3f1daa9971109d8f7b4d9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-22 15:41:02 -0500

    set up php server and refator AJAX code

--- message.php
+++ message.php
@@ -1,5 +1,18 @@
 <?php
-    
+    if($_SERVER['REQUEST_METHOD'] === "POST"){
+        $username = $_POST['username'];
+        $comment = $_POST['comment'];
+        if(!empty($username) && !empty($comment)){
+            echo "<p>Hi, $username your comment has been received successfully</p>";
+            echo "<p>Here is the comment that you have entered: $comment</p>";
+        } else {
+            echo "<p>please fill in all fields in the form!</p>";
+        }
+    }
+    else {
+           echo "<p>oops something went wrong</p>";
+    }
+
 ?>
 
 <!-- command to run server: php -S localhost:8000 -->
